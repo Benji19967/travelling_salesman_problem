5e8209a97a85b1d78cd5bb90ceb0cadbdb786f85
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-08-02 20:41:22 +0200

    Add loading coords from tsplib file

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
+import numpy as np
+
+
 def read_data(filepath: str):
+    x_coords = []
+    y_coords = []
     with open(filepath, "r") as f:
         for line in f:
             if line[0].isdigit():
                 v, x, y = line.strip().split(" ")
-                print(v, x, y)
+                x_coords.append(float(x))
+                y_coords.append(float(y))
+
+    return np.array([x_coords, y_coords])
 
 
 def main():
-    read_data(filepath="./data/dj38.tsp")
+    cities = read_data(filepath="./data/dj38.tsp")
+    print(cities)
+    print(cities[:, 0])
 
 
 if __name__ == "__main__":
